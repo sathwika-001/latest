25745d81c9426ba3608b6647e0e0a01d2510bc16
sathwika.ch <2411CS010096@mallareddyuniversity.ac.in>
2025-07-28 12:22:26 +0530

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import VoatingApp from './VoteApp/VoatingApp.jsx'
+// import VoatingApp from './VoteApp/VoatingApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
