7d16c545668decb44d47e10045cd1c1416d58adb
sathwika.ch <2411CS010096@mallareddyuniversity.ac.in>
2025-07-28 10:35:23 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import VoatingApp from './VoteApp/VoatingApp.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <VoatingApp />
+  </StrictMode>,
+)
